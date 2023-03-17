59b5aedb8042a5e775a964444330407ab27a222d
ramonprata <sis.ramon.prata@gmail.com>
2023-03-17 02:27:18 -0300

    2 files modified

--- App.tsx
+++ App.tsx
@@ -6,23 +6,23 @@ type Vertice = { left: number; top: number };
 
 const vertices = [
   { left: DEFAULT_SIZE / 2 - 5, top: -10 },
-  { left: -10, top: DEFAULT_SIZE * 0.4 },
-  { left: DEFAULT_SIZE, top: DEFAULT_SIZE * 0.4 },
-  { left: DEFAULT_SIZE * 0.2, top: DEFAULT_SIZE },
   { left: DEFAULT_SIZE * 0.8, top: DEFAULT_SIZE },
+  { left: DEFAULT_SIZE * 0.2, top: DEFAULT_SIZE },
+  { left: -10, top: DEFAULT_SIZE / 3 },
+  { left: DEFAULT_SIZE, top: DEFAULT_SIZE / 3 },
 ];
-let currentPoint = vertices[4];
+let currentPoint = vertices[0];
 let lastVertice = 0;
 
 const getRandomVertice = (lastVerticeIndex: number) => {
-  let selectedVertice = Math.floor(Math.random() * 4) + 1;
+  let selectedVertice = Math.floor(Math.random() * vertices.length);
   while (selectedVertice === lastVerticeIndex) {
-    selectedVertice = Math.floor(Math.random() * 4) + 1;
+    selectedVertice = Math.floor(Math.random() * vertices.length);
   }
   return selectedVertice;
 };
 
-const getHalfPoint = (selectedVertice: Vertice, currentPoint: Vertice) => {
+const getHalfPoint = (currentPoint: Vertice, selectedVertice: Vertice) => {
   return {
     top: (currentPoint.top + selectedVertice.top) / 2,
     left: (currentPoint.left + selectedVertice.left) / 2,
@@ -31,18 +31,21 @@ const getHalfPoint = (selectedVertice: Vertice, currentPoint: Vertice) => {
 
 const dots: Vertice[] = [];
 
+
+
 export default function App() {
   const [timer, setTimer] = React.useState(1);
 
+  const newDot = getHalfPoint(currentPoint, vertices[lastVertice]);
   const verticeIdx = getRandomVertice(lastVertice);
-  const newDot = getHalfPoint(vertices[verticeIdx], currentPoint);
   dots.push(newDot);
   lastVertice = verticeIdx;
   currentPoint = { ...newDot };
+
   React.useEffect(() => {
     const timerId = setInterval(() => {
       return setTimer(timer + 1);
-    }, 300);
+    }, 10);
     return () => clearInterval(timerId);
   });
 
@@ -53,7 +56,9 @@ export default function App() {
           <div key={idx} className="vertice" style={{ ...v }}></div>
         ))} */}
         {dots.map((d, idx) => (
-          <span className="dot" key={idx} style={{ ...d }} />
+          <span className="dot" key={idx} style={{ ...d }}>
+            *
+          </span>
         ))}
       </div>
     </div>
